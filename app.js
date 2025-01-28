4a2df5ed83425439631654545c33fe7513277490
3SSF <muhaddithumar@gmail.com>
2025-01-28 14:10:02 +0000

    made non-path arguments stackable, added an option to disable name logging besides privacy mode

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ let opts = {
     p : false,  // Privacy mode flag
     P : null,   // Phrases file path
     H : null,    // Hangup file path
-    N : "names",
+    N : "names", // Names file path
+    n : false    // Disable name logging flag
 };
 
 const defaultDir = __dirname;
@@ -21,22 +22,36 @@ for (let i = 0; i < args.length; i++) {
     
     if (arg.startsWith('-')) {
         if (arg === '-h') {
-            console.log('payphone-bot 2000 manual\narguments:\n\t-p - Privacy mode: hides full username, only displaying first 2 letters of username in console\n\t-l - Log to console: whether or not the messages, and responses should be logged to console\n\t-P <file> - Custom file path for phrases\n\t-H <file> - Custom file path for hangup');
+            console.log('payphone-bot 2000 manual\narguments:\n\t-p - Privacy mode: hides full username, only displaying first 2 letters of username in console\n\t-l - Log to console: whether or not the messages, and responses should be logged to console\n\t-P <file> - Custom file path for phrases\n\t-H <file> - Custom file path for hangup\n\t-n - Disable name logging\n\t-N <file> - Custom file path for names');
             process.exit();
-        } else if (arg === '-H') {
-            if (i + 1 < args.length) {
+        }
+
+        else if (arg === '-H' || arg === '-P' || arg === '-N') {
+            if (i + 1 < args.length && args[i + 1].startsWith('-')) {
+                console.error(`Error: Path flags (-P, -H, -N) cannot be stacked with other flags.`);
+                process.exit(1);
+            }
+            if (arg === '-H') {
                 opts.H = args[i + 1];
-                i++;
-            }
-        } else if (arg === '-P') {
-            if (i + 1 < args.length) {
+            } else if (arg === '-P') {
                 opts.P = args[i + 1];
-                i++;
+            } else if (arg === '-N') {
+                opts.N = args[i + 1];
             }
-        } else if (arg === '-p') {
-            opts.p = true;
-        } else if (arg === '-l') {
-            opts.l = false;
+            i++;
+        }
+
+        else if (arg.includes('p') || arg.includes('l') || arg.includes('n')) {
+            if (arg.includes('P') || arg.includes('H') || arg.includes('N')) {
+                console.error(`Error: Path flags (-P, -H, -N) cannot be stacked with other flags.`);
+                process.exit(1);
+            }
+            
+            for (let char of arg.slice(1)) {
+                if (char === 'p') opts.p = true;
+                else if (char === 'l') opts.l = true;
+                else if (char === 'n') opts.n = true;
+            }
         } else {
             console.error(`Invalid argument: ${arg}. Cannot stack arguments.`);
             process.exit(1);
@@ -49,7 +64,6 @@ const pMessages = fc.split(/\r?\n/);
 
 fc = fs.readFileSync(opts.H || 'hangup', 'utf8');
 const endCallMessages = fc.split(/\r?\n/);
-
 
 async function updateFile(message, N) {
     try {
@@ -93,12 +107,12 @@ client.on("messageCreate", async (message) => {
         if (!opts.l) return;
         if (!opts.p) {
             console.log(`${message.author.username} : `, message.content, " > ", pMessages[randomIndex]);
-            updateFile(message, "names");
+            if (!opts.n) {  // Only log names if '-n' isn't set
+                updateFile(message, opts.N);
+            }
         } else {
             console.log(`${message.author.username[0]}${message.author.username[1]}.. : `, message.content, " > ", pMessages[randomIndex]);
-
         }
-        
     }
 });
 
@@ -108,7 +122,7 @@ client.on("ready", () => {
 
 require("dotenv").config();
 const token = process.env.token
-// put your token in a file named .env
 client.login(token).catch((err) => {
     console.error("Failed to log in:", err);
 });
+
